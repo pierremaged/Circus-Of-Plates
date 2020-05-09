b5b6b852700fad512de80f6b171a6c4d3f19954f
pierremaged <48360502+pierremaged@users.noreply.github.com>
2020-05-09 18:35:02 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import eg.edu.alexu.csd.oop.game.ScreenResolution;
 
 class Main {
     public static void main(String[] args) {
-
         ScreenResolution resolution = new ScreenResolution();
         Mainmenu mainMenu = new Mainmenu(resolution);
         mainMenu.start();
